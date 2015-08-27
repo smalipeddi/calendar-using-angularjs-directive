016c998c134275e84685e529e74cbc8596e941b1
smalipeddi <sunitha_malipeddi@yahoo.com>
2015-08-27 16:35:45 -0400

    calendar application using angular directive

--- app/app.js
+++ app/app.js
@@ -1,3 +1,65 @@
-angular.module('calendarDemoApp', []);
+angular.module("MyApp", [])
+  .controller('AppCtrl', function($scope) {
+    var today = new Date();
+    console.log(today);
+    var year = today.getFullYear();
+    console.log(year);
+    var month = today.getMonth();
 
-// your controller and directive code go here
+    $scope.date = {
+      year : year,
+      month : month
+    };
+
+    $scope.isCurrentMonth = function(date) {
+      return date.getMonth() == $scope.date.month;
+    }
+
+    $scope.months = ('january,february,march,april,' +
+                    'may,june,july,august,' +
+                    'september,october,november,december').split(',');
+
+    var years = [];
+    var then = year - 100;
+    for(var i=then;i<=year;i++) {
+      years.push(i);
+    }
+
+    $scope.years = years;
+
+    $scope.$watchCollection('date', function(date) {
+      $scope.currentDate = new Date(date.year, date.month, 1);
+    });
+  })
+  .directive('myCalendar', function() {
+    return {
+      terminal: true,
+      priority : 1000,
+      transclude : 'element',
+      link : function(scope, element, attrs, ctrl, transclude) {
+        var containerScope = scope.$new(); 
+        var container = angular.element('<div></div>');
+        container.addClass('calendar-container');
+        element.after(container);
+
+        scope.$watch(attrs.myCalendar, function(date) {
+          if(!date) return;
+
+          var range = CalendarRange.getMonthlyRange(date);
+
+          containerScope.$destroy();
+          containerScope = scope.$new();
+          container.html('');
+
+          angular.forEach(range.days, function(day) {
+            var newScope = containerScope.$new();
+            newScope.day = day;
+            transclude(newScope, function(newElement) {
+              newElement.addClass('calendar-cell');
+              container.append(newElement);
+            });
+          });
+        });
+      }
+    }
+  });
